238d61a547e265aba48bbd7cfd40d3696f74edf4
Laura Figueredo <laura.figueredo02@uptc.edu.co>
2025-09-13 10:17:09 -0500

    Step 1_ Configuración de Eureka

---
+++ src/test/java/org/example/load_balance_taller/LoadBalanceTallerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.load_balance_taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalanceTallerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
